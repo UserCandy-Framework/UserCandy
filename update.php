d0193d54f7ae9270a49c633040474c4aaf32917a
DaVaR <davar@thedavar.net>
2019-12-14 06:22:57 -0600

    Getting ready for in framework updates

---
+++ update.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* UserCandy Framework Updater
+*
+* UserCandy
+* @author David (DaVaR) Sargent <davar@usercandy.com>
+* @version 1.0.0
+*/
+
+/** Add Data needed to Database - Oldest Top**/
